d362a154b6eb0a6e7bb4df85fa738b1ccb7577d3
Janez Sedeljšak <janez.sedeljsak@gmail.com>
2023-11-10 21:25:14 +0100

    added heartbeat route and minor fixes

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import db
 from auth import auth_bp
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbuser:postgres@localhost/image-metadata'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbuser:postgres@workclock-db:5432/image-metadata'
 app.register_blueprint(auth_bp)
 
 db.init_app(app)
 
+@app.route('/heartbeat')
+def heartbeat():
+    return jsonify({'hello': 'world'})
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+    #with app.app_context():
+    #    db.create_all()
